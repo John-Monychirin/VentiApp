04b37bdd7332812da78bc429a66afaf66a22f4e2
John-Monychirin <khalehoria@gmail.com>
2020-04-22 12:15:52 +0300

    VentiApp Interfaces updated activities with database

---
+++ app/src/main/java/com/example/ventiapp/DatabaseHelper.java
@@ -0,0 +1,10 @@
+package com.example.ventiapp;
+
+
+        import android.database.sqlite.SQLiteOpenHelper;
+        import android.provider.BaseColumns;
+
+public class DatabaseHelper extends SQLiteOpenHelper implements BaseColumns {
+
+public class
+}
